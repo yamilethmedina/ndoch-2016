d75d55d7cfad7e66398c8dc9cd30e1abb70a43b2
Micah Resnick <resnickmicah@gmail.com>
2016-06-04 15:42:25 -0400

    Displaying UFlorida map data with names and addresses

--- js/load-map.js
+++ js/load-map.js
@@ -7,7 +7,8 @@ $(function() {
     });
 
     function populateMap(response) {
-        var mymap = L.map('main-map').setView([25.78, -80.26], 10);
+        var mymap = L.map('main-map').setView([25.82, -80.23], 12);
+        var addressDisplay = '';
 
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
         maxZoom: 18,
@@ -18,7 +19,12 @@ $(function() {
         }).addTo(mymap);
         
         response.results.forEach(function(development) {
-            L.marker([development.latitude, development.longitude]).addTo(mymap);
+            addressDisplay = '<b>' + development.name + '</b>' + '<br>' +
+                development.address + '<br>' +
+                development.city + ', '+ development.state + ' ' + development.zip + '<br>';
+            L.marker([development.lat, development.lng])
+            .addTo(mymap)
+            .bindPopup(addressDisplay);
         });
     }
 });
